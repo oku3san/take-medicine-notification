d47876566bdf81af1addb12e03129e18473ba156
oku3san <takaya0303@gmail.com>
2021-09-02 22:39:20 +0900

    Add lambda

--- lib/take-medicine-notification-stack.ts
+++ lib/take-medicine-notification-stack.ts
@@ -1,9 +1,16 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
 
 export class TakeMedicineNotificationStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const cronFunction = new lambda.DockerImageFunction(this, 'cronFunction', {
+      code: lambda.DockerImageCode.fromImageAsset("src/lambda/cron"),
+      memorySize: 128,
+      environment: {
+      },
+    });
   }
 }
---
+++ src/lambda/cron/index.ts
@@ -0,0 +1,15 @@
+import * as Lambda from 'aws-lambda';
+import * as Line from "@line/bot-sdk";
+import * as Types from "@line/bot-sdk/lib/types";
+import * as AWS from "aws-sdk";
+
+console.log('Loading function');
+
+export const handler: Lambda.Handler = async (event, context: Lambda.Context) => {
+  console.log('Received event:', JSON.stringify(event, null, 2));
+  console.log('value1 =', event.key1);
+  console.log('value2 =', event.key2);
+  console.log('value3 =', event.key3);
+  return event.key1;  // Echo back the first key value
+  throw new Error('Something went wrong');
+};
