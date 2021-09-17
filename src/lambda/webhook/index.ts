7ddf1db5fc5c7fa076fa5e33f828e1acc69de8de
oku3san <takaya0303@gmail.com>
2021-09-18 00:25:57 +0900

    Add modifyData and change DynamoDB schema for webhook lambda

--- src/lambda/webhook/index.ts
+++ src/lambda/webhook/index.ts
@@ -47,10 +47,10 @@ const eventHandler = async (event: Line.WebhookEvent): Promise<any> => {
       // DynamoDB から情報取得
       const messages = await fetchAllData()
         .then((data: any) => {  // 取得したデータをもとに処理
-          // Hour 順にソート
+          // TimetableId 順にソート
           let items: string[] = data.Items
           items.sort((a: any, b: any) => {
-            if (a.Hour < b.Hour) {
+            if (a.TimetableId < b.TimetableId) {
               return -1;
             } else {
               return 1;
@@ -73,7 +73,31 @@ const eventHandler = async (event: Line.WebhookEvent): Promise<any> => {
       // Line message 送信
       const lineMessage: Types.Message = {type: "text", text: messages.join('\n')}
       return await sendMessage(event.replyToken, lineMessage)
-      break
+    }
+    case event.message.text.startsWith('登録') && event.message.text: {
+      // 空白で split[登録,朝,11]のようになる
+      const messages: string[] = event.message.text.split('　')
+      switch (messages[1]) {
+        case '朝' : {
+          const timetableId: number = 0
+          await modifyData(timetableId, Number(messages[2]))
+          break
+        }
+        case '昼' : {
+          const timetableId: number = 1
+          await modifyData(timetableId, Number(messages[2]))
+          break
+        }
+        case '夜' : {
+          const timetableId: number = 2
+          await modifyData(timetableId, Number(messages[2]))
+          break
+        }
+      }
+
+      // Line message 送信
+      const lineMessage: Types.Message = {type: "text", text: '登録実施'}
+      return await sendMessage(event.replyToken, lineMessage)
     }
     default: {
       const lineMessage: Types.Message = {type: "sticker", packageId: '8515', stickerId: '16581263'};
@@ -141,5 +165,34 @@ const fetchAllData = async (): Promise<any> => {
   }
   catch(e) {
     console.error("データ取得失敗", JSON.stringify(e))
+  }
+}
+
+// DynamoDB のデータ更新
+const modifyData = async (timetableId: number, hour: number, status?: number): Promise<any> => {
+  console.log('データ更新開始')
+  setDynamodbOptions()
+
+  // 特定タイムテーブルの時間を更新する
+  const params = {
+      TableName: tableName,
+      Key: {
+        'UserId': userId,
+        'TimetableId': timetableId
+      },
+      UpdateExpression: 'SET #Hr = :Hr ',
+      ExpressionAttributeNames: {
+        '#Hr': 'Hour',
+      },
+      ExpressionAttributeValues: {
+        ':Hr': hour,
+      }
+    }
+  try {
+    // 特定タイムテーブルの時間を更新する
+    return await documentClient.update(params).promise()
+  }
+  catch(e) {
+    console.error("データ更新失敗", JSON.stringify(e))
   }
 }
